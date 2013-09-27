7f7e5a58996f0ec16cd5e413abc31f04c70e6541
Dag Martin Mikkelsen <dmi@1x-193-157-246-225.uio.no>
2013-09-27 20:36:04 +0200

    Refactored Enemy Stuff

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-epletd
-======
+# EpleTD
+
+## Documentation
--- js/enemy.js
+++ js/enemy.js
@@ -1,54 +1,23 @@
-function Enemy() {
-    this.name;
+function Enemy(config) {
+    this.name        = config.name;
+    this.color       = config.color;
+    this.reward      = config.reward;
+    this.penalty     = config.penalty;
+    this.density     = config.density;
+    this.torque      = config.torque;
+    this.gravity     = config.gravity;
+    this.damping     = config.damping;
+    this.friction    = config.friction;
+    this.restitution = config.restitution;
+    this.health      = config.health;
+
     this.shapeObject;
-    this.color;
-    this.reward;
-    this.penalty;
-    this.density;
-    this.torque;
-    this.gravity;
-    this.damping;
-    this.friction;
-    this.restitution;
-    this.health;
-}
-
-function createEnemy(enemyType) {
-    var enemy;
-    var config;
-
-    switch (enemyType) {
-        case 'normal':
-            config = EnemyConfig.normal;
-            break;
-    }
-
-    // If we don't have a valid enemy type throw error
-    if (!config) {
-        console.log('Enemy type not valid. Enemy not created. *sadface*');
-    }
-
-    // Load all config
-    enemy = new Enemy();
-    enemy.name = config.name;
-    enemy.color = config.color;
-    enemy.reward = config.reward;
-    enemy.penalty = config.penalty;
-    enemy.torque = config.torque;
-    enemy.gravity = config.gravity;
-    enemy.damping = config.damping;
-    enemy.friction = config.friction;
-    enemy.restitution = config.restitution;
-    enemy.health = config.health;
 
     // Create shape
     switch (config.shape) {
         case 'circle':
-            enemy.shapeObject = new iio.Circle();
-            enemy.shapeObject.parent = enemy;
+            this.shapeObject = new iio.Circle();
+            this.shapeObject.parent = this;
             break;
     }
-
-    // Return that shit mothafucka!
-    return enemy;
 }
