dae68f25b7ca1573a72b52df99cfe7f15f0f8925
Dag Martin Mikkelsen <dmi@1x-193-157-246-225.uio.no>
2013-09-27 19:47:54 +0200

    More enemy config

--- config/EnemyConfig.js
+++ config/EnemyConfig.js
@@ -1,9 +1,16 @@
-
-var configEnemies = {
+EnemyConfig = {
     "normal": {
         "name": "Normal",
-        "goldValue": 5,
+        "shape": "circle",
+        "color": "blue"
+        "reward": 5,
+        "penalty": 5,
+        "density": 0.5,
         "torque": 0.5,
         "gravity": 0.5,
+        "damping": 0.1,
+        "friction": 0.1,
+        "restitution": 0.1,
+        "health": 100
     }
 }
