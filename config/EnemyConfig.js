ac3eb9aa3d604a4585933ddefc5bf66b08fbee0d
Dag Martin Mikkelsen <dmi@Dags-MacBook-Pro.local>
2013-09-28 17:18:24 +0200

    Changed settings for enemy

--- config/EnemyConfig.js
+++ config/EnemyConfig.js
@@ -1,14 +1,14 @@
 EnemyConfig = {
     "normal": {
         "name": "Normal",
-        "shape": "circle",
-        "color": "blue",
+        "shape": "square",
+        "color": "red",
         "reward": 5,
         "penalty": 5,
         "density": 0.5,
         "damping": 0.1,
         "friction": 0.1,
-        "restitution": 0.1,
+        "restitution": 0.8,
         "health": 100
     }
 }
