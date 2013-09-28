191c1195ace1813df7926a7e45c34a5db4f2603d
Dag Martin Mikkelsen <dmi@Dags-MacBook-Pro.local>
2013-09-28 18:15:30 +0200

    Towershit

--- js/Tower.js
+++ js/Tower.js
@@ -1,13 +1,26 @@
 function Tower(x, y, config) {
-    this.config = config;
-    this.x = x;
-    this.y = y;
+    this.fireInterval = config.fireInterval;
+    this.lastShot = new Date().getTime();
 
 
     this.body = TD.createBox2DBody(x, y, config.box2d);
     this.body.parent = this;
 
-    this.fire = function(targetEnemy) {
+    this.update = function() {
+        // Test if fire is off cooldown.
+        var currentTime = new Date().getTime();
+        if ((currentTime - this.lastShot) >= this.fireInterval) {
+            this.fire();
+            this.lastShot = currentTime;
+        }
+    }
+
+    this.fire = function() {
+        // Find closest enemy
+        var enemies = TD.io.getGroup('enemies');
+        enemies.forEach(function(enemy, index) {
+                        
+        });
         new Projectile(this.x, this.y, this.config.projectile, targetEnemy);
     };
 }
