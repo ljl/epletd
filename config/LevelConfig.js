1ae512e6e930e1fdba9e5da7b04cace5303ec621
Lasse Jul-Larsen <ljl@enonic.com>
2013-09-29 15:13:22 +0200

    Levelchanges

--- config/LevelConfig.js
+++ config/LevelConfig.js
@@ -3,24 +3,28 @@ LevelConfig = [
         levelTime: 5000,
         level: 1,
         spawnTime: 700,
-        startDelay: 5000
+        startDelay: 5000,
+        enemyConfig: EnemyConfig.normal
     },
     {
         levelTime: 7000,
         level: 2,
         spawnTime: 500,
-        startDelay: 3000
+        startDelay: 3000,
+        enemyConfig: EnemyConfig.normal
     },
     {
         levelTime: 10000,
         level: 3,
         spawnTime: 300,
-        startDelay: 2000
+        startDelay: 2000,
+        enemyConfig: EnemyConfig.normal
     },
     {
         levelTime: 15000,
         level: 4,
         spawnTime: 100,
-        startDelay: 1000
+        startDelay: 1000,
+        enemyConfig: EnemyConfig.normal
     }
 ];
--- js/Level.js
+++ js/Level.js
@@ -2,6 +2,7 @@ function Level(config) {
     this.level = config.level;
     this.levelTime = config.levelTime;
     this.spawnTime = config.spawnTime;
+    this.enemyConfig = config.enemyConfig;
     this.text = TD.io.addToGroup('GUI',new iio.Text('Level ' + this.level, (MapConfig.cell.x * MapConfig.cols), 100))
         .setFont('30px Consolas')
         .setFillStyle('black');
@@ -12,7 +13,7 @@ function Level(config) {
         var me = this;
         this.text.setText('Level ' + this.level);
         this.levelIntervalId = setInterval(function () {
-            var enemy = new Enemy((MapConfig.cell.x * MapConfig.cols / 2), MapConfig.cell.y, EnemyConfig.normal);
+            var enemy = new Enemy((MapConfig.cell.x * MapConfig.cols / 2), MapConfig.cell.y, me.enemyConfig);
             TD.io.addToGroup('enemies', enemy.body);
         }, this.spawnTime);
 
